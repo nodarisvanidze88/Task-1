c5c9bf7c967cbffa461bb138c1953dea85e07c17
nodarisvanidze88 <99223872+nodarisvanidze88@users.noreply.github.com>
2023-03-07 20:17:57 +0400

    Update Hello V3.1.py

--- Hello V3.1.py
+++ Hello V3.1.py
@@ -1,8 +1,20 @@
-# answer to user with different text
+    # answer to user with different text
     # import random modul
 import random
     # get user name
 name = input("What is your name? ")
+    # function for remove extra spaces inside the text
+def ver2(inputedText):
+    # split text by separator " "
+    text = inputedText.split(" ")
+    # create empty list
+    result = []
+    # run loop to create new list without spaces
+    for item in text:
+        if item != "":
+            result.append(item)
+    return " ".join(result)
+name = ver2(name)
     # create different versions of answer
 ver1 = (f"Hello {name}, Nice to meet you.").strip().title()
 ver2 = (f"Hi {name}, thank you for invitation").strip().title()
