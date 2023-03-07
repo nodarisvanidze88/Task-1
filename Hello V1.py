2c43b69e36bcfb69a355fa86d89685ae71a2bac9
nodarisvanidze88 <99223872+nodarisvanidze88@users.noreply.github.com>
2023-03-07 20:18:27 +0400

    Update Hello V1.py

--- Hello V1.py
+++ Hello V1.py
@@ -4,7 +4,21 @@ name = input("What is your name? ")
 name = name.strip()
 # first symbol uppercase
 name = name.title()
+
+# function for remove extra spaces inside the text
+def ver2(inputedText):
+    # split text by separator " "
+    text = inputedText.split(" ")
+    # create empty list
+    result = []
+    # run loop to create new list without spaces
+    for item in text:
+        if item != "":
+            result.append(item)
+    return " ".join(result)
+#put in function user's input value
+name = ver2(name)
+
 # print result with different code but as the same
 print("Hello", name + ",nice to meet you")
 print(f"Hello {name},nice to meet you")
-# problems: what if the user will imput extra spaces between words for example "test      test"
